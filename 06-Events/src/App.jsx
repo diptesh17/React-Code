4b75589452a75d7c45962a476357390b369375bb
Diptesh <dipteshdde22@gmail.com>
2024-10-13 18:07:49 +0530

    Events

---
+++ 06-Events/src/App.jsx
@@ -0,0 +1,38 @@
+const First = () => {
+  return <button onClick={ () => { console.log('You clicked me !')}}></button>
+};
+
+const Second = () => {
+  return <p onCopy={ () =>  {alert('Stop copying me !')}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor odio nostrum aliquam assumenda fugit sint reprehenderit facilis eius cupiditate vel molestiae omnis, quisquam nihil dolorem obcaecati, alias fugiat. Alias, perspiciatis.
+  Mollitia voluptatibus modi aliquam. Magnam libero explicabo dolores dolor molestiae ab quod corporis, amet eius culpa repudiandae veritatis sequi praesentium a earum inventore consequuntur rerum qui quisquam voluptatem corrupti debitis.
+  Expedita magni odit ut natus quisquam ea? Doloribus unde consequatur temporibus vitae expedita officia porro repellat reprehenderit voluptas! Labore provident voluptatibus, aliquam animi nesciunt adipisci voluptate aliquid quis perferendis praesentium.
+  Modi repellat doloremque animi nulla accusamus distinctio quae libero in temporibus tempore maiores voluptas quam magni voluptatum labore, cupiditate veritatis. Cupiditate recusandae accusamus dignissimos, enim consectetur fugit facilis libero quia.
+  Accusantium nisi veniam molestiae excepturi, distinctio error cum quisquam aperiam! Dolorum officia temporibus sed impedit hic accusantium, quam asperiores, rerum, est aperiam tempore maiores dicta quo. Ad consequatur molestias consectetur!
+  Quae autem quisquam sapiente modi ipsum. Expedita incidunt placeat cupiditate, fugit dolorum maiores, labore molestiae quae, natus sit ad quis officiis ipsum beatae maxime modi aspernatur facere! Est, exercitationem corrupti?
+  Corrupti voluptate qui pariatur accusamus a, dolore optio cum facere dolorem dolores officiis distinctio aliquid corporis quis rerum? Fuga, iste officia nam sit accusantium blanditiis ex debitis ipsam non autem.
+  Fuga libero repudiandae minus, ipsa consectetur, id non delectus, accusantium adipisci maiores unde quos eius optio rem? Perspiciatis voluptas nesciunt non animi ducimus, nisi id ipsam quisquam, tempore in quo!
+  Quas, hic praesentium! At beatae architecto explicabo alias est magni recusandae velit molestias labore? Cumque eum cum assumenda sequi atque labore, id, obcaecati numquam corrupti libero eveniet quidem in dolore.
+  Reprehenderit autem sequi cupiditate voluptate eveniet magni quod eaque iure at voluptas impedit culpa placeat sapiente, temporibus, commodi fugiat molestias quasi! Culpa enim quae deserunt laudantium ex assumenda maxime doloribus!
+  Recusandae quo, necessitatibus debitis voluptates id vel est cupiditate blanditiis. Ipsa officiis nisi consequuntur id vitae voluptatibus ipsum, incidunt rerum nostrum rem aperiam odit quos quas ad assumenda sapiente hic.
+  Inventore esse ex itaque explicabo reiciendis doloribus error nisi laborum nobis. Quisquam rerum, a aut deserunt alias harum? Maiores assumenda aperiam sequi porro officia possimus? Ipsa ut vel ex incidunt?
+  Dicta fugiat recusandae quaerat iure totam perferendis rerum, officiis temporibus neque tempora voluptatibus dignissimos qui a deserunt aut necessitatibus officia obcaecati eos debitis minus. Quod consequatur dolorem atque alias accusamus.
+  Animi voluptates atque velit ea iusto deleniti voluptate, minus labore accusamus maiores quibusdam quod dolorum eaque nam recusandae quidem modi qui ab. Amet quod magnam ipsum hic, odio est in.
+  Ea nostrum sequi architecto sit ullam, nesciunt odio totam voluptatum eligendi voluptate temporibus, aliquam velit et esse. Consequuntur qui perspiciatis eos laborum molestias, at, reprehenderit beatae sed, molestiae corporis cumque.
+  Libero odit esse voluptas ullam placeat, laudantium optio aperiam! Illo eos, mollitia quibusdam ad minima iste impedit officiis laudantium. Eos doloremque et ipsam voluptatibus quo autem qui aliquam repudiandae eligendi.
+  Officiis illo qui recusandae explicabo quis incidunt delectus nulla amet commodi sequi, unde in numquam itaque fugiat eum labore alias veritatis earum, tempora culpa, ratione id. Dolor perferendis ab quaerat?
+  Non dolore sed adipisci, ex laboriosam ea ipsa libero deserunt tenetur minus similique, fugit iste molestias reiciendis odit quibusdam eius magnam temporibus assumenda, dolor aliquid? Accusamus molestias obcaecati esse quos.
+  Incidunt, praesentium rerum. Adipisci consectetur vel voluptas perspiciatis explicabo expedita tenetur ratione aut autem temporibus. Facilis natus soluta, eum, quibusdam impedit consectetur, excepturi iste voluptatem modi recusandae blanditiis delectus. Quibusdam!
+  Quasi, amet placeat. Odit neque labore consequatur molestias sit quia ut, illo perferendis! Odio obcaecati, alias maiores cumque et natus. Ad ipsum impedit ex iusto quam rerum atque quod aut!</p>
+};
+
+const App = () => {
+  return (
+    <section>
+      <First/>
+      <Second/>
+
+    </section>
+  )
+}
+
+export default App
