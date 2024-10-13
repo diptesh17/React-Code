297ef549bcc6b3e65bb38323d35a1634e6f3eb8c
Diptesh <dipteshdde22@gmail.com>
2024-10-13 19:04:23 +0530

    CRUD on state

--- 07-State/src/App.jsx
+++ 07-State/src/App.jsx
@@ -4,14 +4,42 @@ import { useState } from "react"
 
 const App = () => {
 
+  const [friends , setFriends] = useState(['Alex' , 'John']);
+
   const [ counter , setCounter] = useState(0);
 
   const handler = () =>{
     setCounter(counter+1);
   }
 
+  const addNew = () =>
+  {
+    return setFriends([...friends , "Ronyy"]);
+  }
+
+  const removeOne = () =>
+    {
+      return setFriends( friends.filter( (f) => f !== 'John'));
+    }
+
+    const updateOne = () =>
+      {
+        return setFriends( friends.map( f => f === "Alex" ? "Alex Bhai" :f));
+      }
+
+
   return (
+    <>
     <button onClick={handler}>Current Count : { counter}</button>
+    {
+      friends.map( f => (
+        <li key={Math.random}>{f}</li>
+      ))
+    }
+    <button onClick={addNew}>Add a friend</button>
+    <button onClick={removeOne}>Remove a friend</button>
+    <button onClick={updateOne}>Update a friend</button>
+    </>
   )
 }
 
