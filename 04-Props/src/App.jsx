1c7edf7381eaeadb78b64fce396b49a162b75f10
Diptesh <dipteshdde22@gmail.com>
2024-10-13 11:04:10 +0530

    Props

---
+++ 04-Props/src/App.jsx
@@ -0,0 +1,11 @@
+import User from "./Components/User"
+
+const App = () => {
+  return (
+    <div>
+      <User name="Dip" roll={20} div="A" />
+    </div>
+  )
+}
+
+export default App
---
+++ 04-Props/src/Components/User.jsx
@@ -0,0 +1,13 @@
+
+// eslint-disable-next-line react/prop-types
+const User = ({name,roll,div}) => {
+  return (
+    <>
+    <p>Name :{name}</p>
+    <p>Roll :{roll}</p>
+    <p>Div :{div}</p>
+    </>
+  )
+}
+
+export default User
