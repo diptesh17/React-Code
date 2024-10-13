a4abbe81eacf270ba8725e4893b1aeb1a52d0d99
Diptesh <dipteshdde22@gmail.com>
2024-10-13 17:35:47 +0530

    styling + react icons

---
+++ 05-styling/src/App.jsx
@@ -0,0 +1,14 @@
+import { IoMdLaptop } from "react-icons/io";
+const App = () => {
+
+  const styles = { color : "white" ,  backgroundColor : "teal" , padding : "2rem"};
+
+  return (
+    <>
+    <div style={ styles}>Inline Styling</div>
+    <IoMdLaptop />
+    </>
+  )
+}
+
+export default App
