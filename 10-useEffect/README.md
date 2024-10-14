ed53d3c6a9dc1b7188a28f3043a72863c1734fec
Diptesh <dipteshdde22@gmail.com>
2024-10-14 14:03:53 +0530

    useEffect with fake api

---
+++ 10-useEffect/README.md
@@ -0,0 +1,4 @@
+# useEffect 
+- Helps to perform side effects like fetching data , directly updating DOM
+- If dependency array isn't mentioned , then it will execute useEffect when component re-renders
+- Dont wrap useEffect inside the conditional rendering , add conditional rendering inside useEffect
---
+++ 10-useEffect/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState , useEffect } from "react";
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const data = await response.json();
+      if (data && data.length) setData(data);
+    }
+    getData();
+  }, []);
+
+  return (
+    <>
+      <ul>
+        {data.map((item) => (
+          <li key={Math.random()}>{item.title}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default App;
