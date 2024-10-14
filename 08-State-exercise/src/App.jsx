cd0d2ce8d82c8e3915b1bd1513a2bd2fab23a379
Diptesh <dipteshdde22@gmail.com>
2024-10-14 09:23:14 +0530

    useState Challanges

--- 08-State-exercise/src/App.jsx
+++ 08-State-exercise/src/App.jsx
@@ -1,12 +1,17 @@
-import Todo from "./Components/Todo"
+import Counter from "./components/Counter";
+import Profile from "./components/Profile";
+import ShoppingList from "./components/ShoppingList";
+import TodoList from "./components/TodoList";
 
-const App = () => {
+function App() {
   return (
     <>
-    <div>Todo  </div>
-    <Todo/>
+      <Counter />
+      <TodoList />
+      <Profile />
+      <ShoppingList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
