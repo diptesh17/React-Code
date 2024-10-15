2004423b73a926538c46ef8a1974c807962db528
Diptesh <dipteshdde22@gmail.com>
2024-10-15 14:33:50 +0530

    Typescript-1

---
+++ 16-Typescript-basics/src/App.tsx
@@ -0,0 +1,11 @@
+import User from "./Components/User"
+
+const App = () => {
+  return (
+    <div>
+      <User name="Dip" age={20}/>
+    </div>
+  )
+}
+
+export default App
---
+++ 16-Typescript-basics/src/Components/User.tsx
@@ -0,0 +1,12 @@
+
+const User = ( {name,age} : {name : string , age : number}) => {
+  return (
+    <div>
+        <h2>{name}</h2>
+        <h2>{age}</h2>
+        
+    </div>
+  )
+}
+
+export default User
