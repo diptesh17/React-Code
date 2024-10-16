a37102211aabe190886a135c73ed9675250f17f3
Diptesh <dipteshdde22@gmail.com>
2024-10-16 09:01:13 +0530

    Radix-UI

---
+++ 20-Radix-UI/src/App.jsx
@@ -0,0 +1,13 @@
+import { Flex, Text, Button } from "@radix-ui/themes";
+
+
+const App = () => {
+  return (
+		<Flex direction="column" gap="2">
+			<Text>Hello from Radix Themes :)</Text>
+			<Button>Let's go</Button>
+		</Flex>
+	);
+}
+
+export default App
